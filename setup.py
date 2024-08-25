ee92305a1dd717b76b46be142b55369b4e2dfeaa
VladosIncorparated <podorvan03@mail.ru>
2024-08-26 08:31:18 +0900

    1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="CRest",
-    version="0.1",
+    version="0.12",
     packages=find_packages(),
     author="V/V",
     description="CRest",
